a75c786419e2ad2e0eb9d5391cbfec857402ac18
BriVandenAkker <47199217+BriVandenAkker@users.noreply.github.com>
2020-06-03 11:06:21 -0700

    misc.

--- portfolio/covid19_sitrep/callbacks.py
+++ portfolio/covid19_sitrep/callbacks.py
@@ -12,7 +12,7 @@ from dash.exceptions import PreventUpdate
 
 
 covid_data = pd.read_csv(os.path.join(os.path.dirname(__file__), "data/covid_country_level_data.csv"))
-covid_data.drop(columns = ['m1_wildcard', 'stringency_index','legacy_stringency_index_for_display', 'legacy_stringency_index', 'age_percent_0_to_14',
+covid_data.drop(columns = ['m1_wildcard', 'stringency_index', 'age_percent_0_to_14',
                            'age_percent_15_to_64'], inplace = True)
 
 
